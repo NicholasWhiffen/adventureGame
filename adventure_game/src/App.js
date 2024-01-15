ed8bc47a0698560762eae9cfc9162b1bee51c4d4
Nick <nicholaswhiffen@hotmail.com>
2024-01-14 21:09:45 -0330

    added general layout of game

--- adventure_game/src/App.js
+++ adventure_game/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h2>Game Title</h2>
+      </div>
+      <div className="body">
+        <div className='game'>
+          <p>text text text</p>
+          <button></button>
+          <button></button>
+
+        </div>
+        <div className='inventory'>
+
+        </div>
+      </div>
     </div>
   );
 }
